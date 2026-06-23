6ec225327fe2d5b14c4ed2464c64b9e751948ad4
MIA Zhang <75258156+miazhang2025@users.noreply.github.com>
2026-06-23 01:43:48 -0400

    config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite'
+import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   root: '.',
   server: { host: true, open: true },
   build: {
     outDir: 'dist',
-    assetsInlineLimit: 0
-  }
+    assetsInlineLimit: 0,
+    rollupOptions: {
+      input: {
+        main: fileURLToPath(new URL('./index.html', import.meta.url)),
+        deck: fileURLToPath(new URL('./deck.html', import.meta.url)),
+      },
+    },
+  },
 })
